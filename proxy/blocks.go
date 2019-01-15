b9103f57b6dae1f4df9c62d6f1fa4c9dcedcbebb
My-Kings <42500519+My-Kings@users.noreply.github.com>
2019-01-14 20:14:40 -0500

    Update blocks.go
    open-reosc-pool

--- proxy/blocks.go
+++ proxy/blocks.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/chainkorea/open-callisto-pool/rpc"
-	"github.com/chainkorea/open-callisto-pool/util"
+	"github.com/My-Kings/open-reosc-pool/rpc"
+	"github.com/My-Kings/open-reosc-pool/util"
 )
 
 const maxBacklog = 3
