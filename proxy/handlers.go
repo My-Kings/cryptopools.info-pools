c1ca4b6915e012b24748398ffb6c6df4647e6993
My-Kings <42500519+My-Kings@users.noreply.github.com>
2019-01-14 20:17:47 -0500

    Update handlers.go
    open-reosc-pool

--- proxy/handlers.go
+++ proxy/handlers.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"errors"
 
-	"github.com/chainkorea/open-callisto-pool/rpc"
-	"github.com/chainkorea/open-callisto-pool/util"
+	"github.com/My-Kings/open-reosc-pool/rpc"
+	"github.com/My-Kings/open-reosc-pool/util"
 )
 
 // Allow only lowercase hexadecimal with 0x prefix
